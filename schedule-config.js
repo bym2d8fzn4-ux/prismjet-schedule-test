d85089512431c08341ed41feea68e099de661974
Adam Barkley <adambarkley@Adams-MacBook-Pro.local>
2026-06-19 10:40:32 -0700

    Connect schedule app to Apps Script backend

--- schedule-config.js
+++ schedule-config.js
@@ -1,1 +1,1 @@
-window.PRISMJET_SCHEDULE_API_URL = "";
+window.PRISMJET_SCHEDULE_API_URL = "https://script.google.com/macros/s/AKfycbwbf2unDtwSMIdC9QK4ALDDqakoDMIFsfpofpZfceQUEUU1zyquD032F29JwcBY00Me/exec";
